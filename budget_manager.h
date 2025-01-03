5e4bcae9c6e02ee91345f0f91307677974ac6f55
Mypos Company <support@mypos.ru>
2025-01-03 15:12:26 +0300

    Init commit

---
+++ budget_manager.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "date.h"
+
+class BudgetManager
+{
+
+    struct DayInfo
+    {
+        double money;
+    };
+
+    std::vector<DayInfo> day_incomes;
+
+public:
+    static const Date START_DATE;
+    static const Date END_DATE;
+
+    BudgetManager() = default;
+    void ComputeIncome(Date from, Date to) const;
+    void PayTax(Date from, Date to);
+    void Earn(Date from, Date to, double income);
+};
---
+++ parser.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "budget_manager.h"
+
+void Parser(std::string_view line);
+
+// Напишите в этом файле код, ответственный за чтение запросов.
